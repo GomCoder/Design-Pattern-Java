020430fb5badc1982fb1dd2f40b3ee36cf128ed3
GomCoder <k2001911@hanmail.net>
2023-02-02 23:51:24 +0900

    chapter09 BridgePattern

---
+++ DesignPattern/src/BridgePattern/DisplayImpl.java
@@ -0,0 +1,8 @@
+package BridgePattern;
+
+public abstract class DisplayImpl {
+	public abstract void rawOpen();
+	public abstract void rawPrint();
+	public abstract void rawClose();
+	
+}
---
+++ DesignPattern/src/BridgePattern2/Display.java
@@ -0,0 +1,7 @@
+package BridgePattern2;
+
+public interface Display {
+	void title(Draft darft);
+	void author(Draft darft);
+	void content(Draft darft);
+}
