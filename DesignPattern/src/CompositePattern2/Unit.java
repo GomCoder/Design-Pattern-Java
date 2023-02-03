dcf6bea062e1228054ed49c4209ff3132547f715
GomCoder <k2001911@hanmail.net>
2023-02-03 21:44:38 +0900

    chapter11 CompositePattern

---
+++ DesignPattern/src/CompositePattern2/Unit.java
@@ -0,0 +1,22 @@
+package CompositePattern2;
+
+public abstract class Unit {
+	private String name;
+	
+	public Unit(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return name + "(" + getSize() + ")";
+		
+	}
+	
+	public abstract int getSize();
+}
